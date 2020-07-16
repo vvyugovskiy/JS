2d8149231e1932793a2b594f8ca4e473cfcb276a
unknown <v.vyugovskiy@mail.com>
2020-07-16 15:57:14 -0400

    hello

---
+++ JS_Day02/WarmUp.js
@@ -0,0 +1,9 @@
+/*
+
+1. Write a function for the palindrome test.
+   If the string is palindrome ==> return true.
+   otherwise ==> return false
+2. Write a function that can remove the duplicates 
+   values from
+   
+*/
